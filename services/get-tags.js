cdeee7dc68d1036f0a30292651953047917347d5
Ruslan Ruya <lorelin97@yandex.ru>
2023-11-11 13:48:58 +0300

    фикс получения тегов

--- services/get-tags.js
+++ services/get-tags.js
@@ -9,6 +9,10 @@ function getTags() {
     // Создать пустой массив для хранения всех тегов
     const allTags = [];
 
+    if(!posts.length) {
+        return {status: 200, message: 'Все теги успешно получены', allTags};
+    }
+
     // Перебрать все посты и собрать все теги в массив
     posts.forEach(post => {
         post.post.tags.forEach(tag => {
