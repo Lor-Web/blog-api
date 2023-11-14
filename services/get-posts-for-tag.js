b49666ee7ab5d19b44871a18f590934844b4014c
Ruslan Ruya <lorelin97@yandex.ru>
2023-11-14 16:28:53 +0300

    обновление данных пользователя перед получением постов по тегу

--- services/get-posts-for-tag.js
+++ services/get-posts-for-tag.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
+const {getRefreshUser} = require('../utils/refresh-user');
+
 
 function getPostsForTag(tag) {
     const dbFile = 'db.json';
 
     // Найти посты в базе данных
     const posts = JSON.parse(fs.readFileSync(dbFile, 'utf8'));
+    const refreshPosts = posts.map(post => {
+        return {
+            post:{ ...post.post},
+            user: {...getRefreshUser(post.user.token)}
+        }
+    })
 
     // Фильтровать посты по тегу
-    const postsWithMatchingTag = posts.filter(post => post.post.tags.includes(tag));
+    const postsWithMatchingTag = refreshPosts.filter(post => post.post.tags.includes(tag));
 
     // Отправить массив постов с указанным тегом в ответе
     return {status: 200, message: 'Посты по тегу успешно получены', posts: postsWithMatchingTag};
